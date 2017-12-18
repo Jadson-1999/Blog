1d463450ea8135873cf47a48d920e491d02b7775
jadson moreira matias filho <32195577+Jadson-1999@users.noreply.github.com>
2017-12-18 13:16:18 -0200

    Initial commit

---
+++ public_html/js/alerta.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function funcao1()
+{
+alert("(17)991349210!");
+}
